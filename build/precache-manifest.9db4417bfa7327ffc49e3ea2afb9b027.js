83c123df85fcdb947cab4769cbe77ac846155b7e
Anastasia <anastasia.satysheva@gmail.com>
2020-05-18 09:29:53 +0530

    Add files via upload

---
+++ build/precache-manifest.9db4417bfa7327ffc49e3ea2afb9b027.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a466c8ff5f854387675681243b1973fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa61b7b8586f93cc5b1",
+    "url": "/static/css/4.eefe970a.chunk.css"
+  },
+  {
+    "revision": "67b4a88313df6a4780a5",
+    "url": "/static/css/5.6e0a76aa.chunk.css"
+  },
+  {
+    "revision": "1b3301ea0de21abf4ecb",
+    "url": "/static/css/main.f2be6034.chunk.css"
+  },
+  {
+    "revision": "c1cdd6f24a0f60d9120f",
+    "url": "/static/js/2.4a2cbda3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4a2cbda3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be60dd46eb43361dce6",
+    "url": "/static/js/3.b6a38d0d.chunk.js"
+  },
+  {
+    "revision": "1fa61b7b8586f93cc5b1",
+    "url": "/static/js/4.64036754.chunk.js"
+  },
+  {
+    "revision": "67b4a88313df6a4780a5",
+    "url": "/static/js/5.c4e89bcd.chunk.js"
+  },
+  {
+    "revision": "1b3301ea0de21abf4ecb",
+    "url": "/static/js/main.14880e71.chunk.js"
+  },
+  {
+    "revision": "bea64bdcc01de4e1bc17",
+    "url": "/static/js/runtime-main.428a046f.js"
+  },
+  {
+    "revision": "bc344a3da06f1e57517e2e236ce6a284",
+    "url": "/static/media/NoteTaker.bc344a3d.png"
+  },
+  {
+    "revision": "58cf0f89c461fc832ac4618d08d0e59c",
+    "url": "/static/media/Resume.58cf0f89.pdf"
+  },
+  {
+    "revision": "110835ca0616c42412c87e4842c07633",
+    "url": "/static/media/Weather.110835ca.png"
+  },
+  {
+    "revision": "3e9b58078247a2cf00d2a94ec898d7a7",
+    "url": "/static/media/hike.3e9b5807.png"
+  },
+  {
+    "revision": "765847182bdeeedbffb9991a8c2ce06d",
+    "url": "/static/media/password.76584718.png"
+  },
+  {
+    "revision": "b7f58aa78b2ce9f9999945cdaf4e90c2",
+    "url": "/static/media/planner.b7f58aa7.png"
+  },
+  {
+    "revision": "bc82aefae56dee123fbfc8c994b69b12",
+    "url": "/static/media/profilePic.bc82aefa.jpg"
+  },
+  {
+    "revision": "ab2884a732bf42e4c85283cda6e013cd",
+    "url": "/static/media/resume.ab2884a7.jpg"
+  },
+  {
+    "revision": "cb00b7460016aae9e37c0b72395a065d",
+    "url": "/static/media/updatePage.cb00b746.png"
+  }
+]);
